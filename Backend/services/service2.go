ac4b064d55f9827ff983b131301e6306e52acbbe
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-09-23 20:48:52 +0300

    connected to db

--- Backend/services/service2.go
+++ Backend/services/service2.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
+	db, err := sql.Open("mysql", "root:spiritbinge@tcp(127.0.0.1:3306)/bebra")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("подключено к базе")
 
 }
