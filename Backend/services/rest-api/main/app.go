0dd85ebc3cf056326c0fda16e222699d74fd2504
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-09-25 21:00:20 +0300

    first get req

--- Backend/services/rest-api/main/app.go
+++ Backend/services/rest-api/main/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -10,32 +11,83 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	JobTitle  string `json:"job_title"`
+	DepID     string `json:"dep_id"`
+}
+
 func main() {
-	//загружаем данные из .env файла
+	// Загружаем данные из .env файла
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Ошибка при загрузке .env файла: %v", err)
 	}
 
-	//получаем значения из .env файла
+	// Подключение к БД
+	db, err := connectToDatabase()
+	if err != nil {
+		log.Fatalf("Ошибка при подключении к базе данных: %v", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Подключение к базе данных успешно установлено")
+
+	r := gin.Default()
+
+	r.GET("/users", func(c *gin.Context) {
+		users, err := getUsers(db)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, users)
+	})
+
+	r.Run(":3002")
+}
+
+func getUsers(db *sql.DB) ([]User, error) {
+	query := `SELECT u.FirstName, u.LastName, u.JobTitle, d.DepTtl
+		FROM Users u
+		LEFT JOIN Departaments d ON u.DepID = d.DepID`
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.FirstName, &user.LastName, &user.JobTitle, &user.DepID); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func connectToDatabase() (*sql.DB, error) {
 	root := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	//подключение к бд
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", root, password, host, port, dbName)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatalf("Ошибка при подключении к базе данных: %v", err)
+		return nil, err
 	}
-	defer db.Close()
 
 	if err := db.Ping(); err != nil {
-		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+		return nil, err
 	}
 
-	fmt.Println("Подключение к базе данных успешно установлено")
-
+	return db, nil
 }
