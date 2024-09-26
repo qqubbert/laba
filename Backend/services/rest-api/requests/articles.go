4695f9562001c7abdabe0cc0227a43018b1b39b0
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-09-26 21:34:31 +0300

    add new get req articleById

--- Backend/services/rest-api/requests/articles.go
+++ Backend/services/rest-api/requests/articles.go
@@ -1,14 +1,19 @@
 package requests
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 type Article struct {
-	ID           int    `json:"id"`
-	Title        string `json:"title"`
-	Completed    bool   `json:"completed"`
-	AuthorId     int    `json:"author_id"`
-	CreatingDate string `json:"creating_date"`
-	HtmlLink     string `json:"html_link"`
+	ID           int      `json:"id"`
+	Title        string   `json:"title"`
+	Completed    bool     `json:"completed"`
+	AuthorId     int      `json:"author_id"`
+	CreatingDate string   `json:"creating_date"`
+	HtmlLink     string   `json:"html_link"`
+	AuthorName   string   `json:"author_name"`
+	Tags         []string `json:"tags"`
 }
 
 func GetAllArticles(db *sql.DB) ([]Article, error) {
@@ -36,6 +41,48 @@ func GetAllArticles(db *sql.DB) ([]Article, error) {
 	return articles, nil
 }
 
-func GetArticleById() {
+func GetArticleById(db *sql.DB, id int) (*Article, error) {
 
+	query := `
+		SELECT a.id, a.title, a.completed, a.author_id, a.creating_date, a.HtmlLink,
+		       CONCAT(u.FirstName, ' ', u.LastName, ' ', u.Surname) AS author_name
+		FROM article a
+		JOIN Users u ON a.author_id = u.ID
+		WHERE a.id = ?
+	`
+	var article Article
+	err := db.QueryRow(query, id).Scan(
+		&article.ID, &article.Title, &article.Completed, &article.AuthorId,
+		&article.CreatingDate, &article.HtmlLink, &article.AuthorName,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil // Пользователь не найден
+		}
+		return nil, err
+	}
+
+	tagsQuery := `
+		SELECT biology, chemistry, it, physics 
+		FROM article_tags 
+		WHERE article_id = ?
+	`
+	var tags []string
+	var biolody, chemistry, it, physics bool
+	if err := db.QueryRow(tagsQuery, article.ID).Scan(&biolody, &chemistry, &it, &physics); err == nil {
+		if biolody {
+			tags = append(tags, "biology")
+		}
+		if chemistry {
+			tags = append(tags, "chemistry")
+		}
+		if it {
+			tags = append(tags, "it")
+		}
+		if physics {
+			tags = append(tags, "physics")
+		}
+	}
+	article.Tags = tags
+	return &article, nil
 }
